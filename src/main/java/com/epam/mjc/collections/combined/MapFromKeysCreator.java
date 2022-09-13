fe65a1f681edb9c12688772dd526b2cb363cbf99
MustafoyevMuhammad <110478566+MustafoyevMuhammad@users.noreply.github.com>
2022-09-13 20:36:20 +0500

    Update MapFromKeysCreator.java

--- src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
+++ src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
@@ -7,16 +7,22 @@ import java.util.Set;
 
 public class MapFromKeysCreator {
     public Map<Integer, Set<String>> createMap(Map<String, Integer> sourceMap) {
-        Map<Integer,Set<String>> map = new HashMap<>();
-        for (Map.Entry<String, Integer> temp : sourceMap.entrySet()) {
-            if(map.containsKey(temp.getKey().length()))
-                map.put(temp.getKey().length(), Set.of(temp.getKey()));
-            else{
-                Set<String> temp2 = new HashSet<>();
-                temp2.addAll(map.get(temp.getKey().length()));
-                temp2.add(temp.getKey());
-                map.put(temp.getKey().length(), temp2);//get(temp.getKey().length())
-            }//Set.of(temp.getKey()).addAll(
+        Map<Integer, Set<String>> map = new HashMap<>();
+        try {
+            
+            for (Map.Entry<String, Integer> temp : sourceMap.entrySet()) {
+                if (map.containsKey(temp.getKey().length()))
+                    map.put(temp.getKey().length(), Set.of(temp.getKey()));
+                else {
+                    Set<String> temp2 = new HashSet<>();
+                    temp2.addAll(map.get(temp.getKey().length()));
+                    temp2.add(temp.getKey());
+                    map.put(temp.getKey().length(), temp2);//get(temp.getKey().length())
+                }//Set.of(temp.getKey()).addAll(
+            }
+        }
+        catch (Exception e){
+            
         }
         return map;
     }

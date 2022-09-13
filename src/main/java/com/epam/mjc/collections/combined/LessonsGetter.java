6e52fdb8c90d40bf29d3b8a0f57e9caf9264ada4
MustafoyevMuhammad <110478566+MustafoyevMuhammad@users.noreply.github.com>
2022-09-13 20:31:32 +0500

    Update LessonsGetter.java

--- src/main/java/com/epam/mjc/collections/combined/LessonsGetter.java
+++ src/main/java/com/epam/mjc/collections/combined/LessonsGetter.java
@@ -6,5 +6,12 @@ import java.util.Set;
 
 public class LessonsGetter {
     public Set<String> getLessons(Map<String, List<String>> timetable) {
+        Set<String> list = new HashSet<>();
+        for (Map.Entry<String, List<String>> temp: timetable.entrySet()) {
+            for (String lesson : temp.getValue()) {
+                list.add(lesson);
+            }
+        }
+        return list;
     }
 }

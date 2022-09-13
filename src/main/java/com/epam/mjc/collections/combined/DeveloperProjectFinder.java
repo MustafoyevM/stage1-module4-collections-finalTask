021b7a649c8e139560414a38d168c64b85f6af71
MustafoyevMuhammad <110478566+MustafoyevMuhammad@users.noreply.github.com>
2022-09-13 20:43:37 +0500

    Update DeveloperProjectFinder.java

--- src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
+++ src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
@@ -20,7 +20,7 @@ public class DeveloperProjectFinder {
         Collections.sort(list, new Comparator<String>() {
             @Override
             public int compare(String o1, String o2) {
-                return o1.length() > o2.length() ? 1: -1;
+                return o1.length() > o2.length() ? -1: 1;
             }
         });
         return list;
